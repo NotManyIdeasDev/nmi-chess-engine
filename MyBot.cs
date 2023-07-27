19dec8fbf977f6929d5a8c13978079d1f4d91126
Gianni Alessandroni <45824238+NotManyIdeasDev@users.noreply.github.com>
2023-07-27 16:00:38 +0200

    Refactored code

--- MyBot.cs
+++ MyBot.cs
@@ -3,10 +3,9 @@ using System;
 
 public class MyBot : IChessBot
 {
-    //Compressed PSTs & piece values taken from "https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function".
-    int[] mgPieceValues = { 82, 337, 365, 477, 1025, 20000 };
-    int[] egPieceValues = { 94, 281, 297, 512, 936, 20000 };
-
+    //Pre-Generated PSTs and constants.
+    int bigNumber = 99999999;
+    int[] mgPieceValues = { 82, 337, 365, 477, 1025, 32000 }; //Pawn, Knight, Bishop, Rook, Queen, King
     static ulong[,] compressedMGPSTs = {
         { 0x0000000000000000, 0xDDFFECE9F11826EA, 0xE6FCFCF6030321F4, 0xE5FEFB0C11060AE7, 0xF20D0615170C11E9, 0xFA071A1F413819EC, 0x627F3D5F447E22F5, 0x0000000000000000 }, //mgPawnTable.
         { 0x97EBC6DFEFE4EDE9, 0xE3CBF4FDFF12F2ED, 0xE9F70C0A131119F0, 0xF304100D1C1315F8, 0xF711133525451216, 0xD13C25415481492C, 0xB7D74824173E07EF, 0x81A7DECF3D9FF195 }, //mgKnightTable.
@@ -16,51 +15,67 @@ public class MyBot : IChessBot
         { 0xF1240CCA08E4180E, 0x0107F8C0D5F00908, 0xF2F2EAD2D4E2F1E5, 0xCFFFE5D9D2D4DFCD, 0xEFECF4E5E2E7F2DC, 0xF71802F0EC0616EA, 0x1DFFECF9F8FCDAE3, 0xBF1710F1C8DE020D }, //mgKingTable.
     };
 
-    int mDepth;
+    int botDepth = 4;
     Move bestMove;
 
     public Move Think(Board board, Timer timer)
     {
-        mDepth = 4;
-        Search(board, mDepth, -99999999, 99999999, board.IsWhiteToMove ? 1 : -1);
+        SearchABNegaMax(board, botDepth, -bigNumber, bigNumber, board.IsWhiteToMove ? 1 : -1);
         return bestMove;
     }
 
-
-    public sbyte GetMGPSTValue(PieceType pieceType, sbyte square, bool white)
+    public int EvaluatePosition(Board board)
     {
-        //Get the corresponding positional value for each square of each pieceType, flip if black.
-        return white ? (sbyte)BitConverter.GetBytes(compressedMGPSTs[(byte)pieceType - 1, (sbyte)Math.Floor((double)(square / 8))])[7 - (square % 8)] :
-            (sbyte)BitConverter.GetBytes(compressedMGPSTs[(byte)pieceType - 1, 7 - (sbyte)Math.Floor((double)(square / 8))])[square % 8];
+        int evaluation = 0;
+        for (int i = 0; ++i < 7;)
+        {
+            PieceList whitePieceList = board.GetPieceList((PieceType)i, true);
+            foreach (Piece whitePiece in whitePieceList)
+            {
+                //Material + piece placement
+                evaluation += mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, whitePiece.Square.Index, true);
+            }
+
+            PieceList blackPieceList = board.GetPieceList((PieceType)i, false);
+            foreach (Piece blackPiece in blackPieceList)
+            {
+                //Material + piece placement
+                evaluation -= mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, blackPiece.Square.Index, false);
+            }
+
+            //Detect bishop pair, give 50 centipawns.
+            if (i == 3)
+                evaluation += (50 * Convert.ToInt16(whitePieceList.Count == 2)) - (50 * Convert.ToInt16(blackPieceList.Count == 2));
+        }
+
+        return evaluation;
     }
 
-    //NegaMax with alpha-beta pruning
-    public int Search(Board board, int depth, int alpha, int beta, int color)
+    public int SearchABNegaMax(Board board, int depth, int alpha, int beta, int color)
     {
         Move[] legalMoves;
-
         if (board.IsInsufficientMaterial() || board.IsRepeatedPosition() || board.FiftyMoveCounter >= 100)
             return 0;
 
-        if (depth == 0 || ((legalMoves = board.GetLegalMoves()).Length == 0))
+        if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
         {
             if (board.IsInCheckmate())
-                return -9999999;
+                return -bigNumber;
 
-            return color * Evaluate(board);
+            return color * EvaluatePosition(board);
         }
 
-        int bestEvaluation = int.MinValue;
+        int bestEvaluation = -bigNumber;
         foreach (Move move in legalMoves)
         {
             board.MakeMove(move);
-            int evaluation = -Search(board, depth - 1, -beta, -alpha, -color);
+            int evaluation = -SearchABNegaMax(board, depth - 1, -beta, -alpha, -color);
             board.UndoMove(move);
 
-            if (bestEvaluation < evaluation)
+            if (evaluation > bestEvaluation)
             {
                 bestEvaluation = evaluation;
-                if (depth == mDepth)
+                if (depth == botDepth)
                     bestMove = move;
             }
             alpha = Math.Max(alpha, bestEvaluation);
@@ -70,40 +85,12 @@ public class MyBot : IChessBot
         return bestEvaluation;
     }
 
-    public int Evaluate(Board board)
+    public int GetMGPSTValue(PieceType pieceType, int square, bool white)
     {
-        int materialCount = 0;
-        int positionalBalance = 0; 
-        for (int i = 0; ++i < 7;)
-        {
-            //TODO: Fix duplicate code? Find a better way to combine black and white.
-            //Get material count and position evaluation for white.
-            PieceList whitePieceList = board.GetPieceList((PieceType)i, true); 
-            foreach (Piece whitePiece in whitePieceList)
-            {
-                materialCount += mgPieceValues[i - 1];
-                positionalBalance += GetMGPSTValue((PieceType)i, (sbyte)whitePiece.Square.Index, true);
-            }
-
-            //Get material count and position evalutaion for black.
-            PieceList blackPieceList = board.GetPieceList((PieceType)i, false);
-            foreach (Piece blackPiece in board.GetPieceList((PieceType)i, false))
-            {
-                materialCount -= mgPieceValues[i - 1];
-                positionalBalance -= GetMGPSTValue((PieceType)i, (sbyte)blackPiece.Square.Index, false);
-            }
-
-
-            //Detect bishop pair (+-0.50) -- cost: 32 tokens.
-            if (i == 3)
-            {
-                positionalBalance += 50 * Math.Sign(Convert.ToInt16(whitePieceList.Count == 2));
-                positionalBalance -= 50 * Math.Sign(Convert.ToInt16(blackPieceList.Count == 2));
-            }
-
-        }
-
-        //TODO: Maybe add some sort of weighting to this? Right now it is a plain sum.
-        return materialCount + positionalBalance;
+        //Flip PSTs for opposite colors.
+        int firstIndex = white ? (sbyte)(square / 8) : (7 - (sbyte)(square / 8));
+        int secondIndex = white ? (7 - (square % 8)) : square % 8;
+        //Get the corresponding positional value for each square of each pieceType, flip if black.
+        return BitConverter.GetBytes(compressedMGPSTs[(byte)pieceType - 1, firstIndex])[secondIndex];
     }
 }
