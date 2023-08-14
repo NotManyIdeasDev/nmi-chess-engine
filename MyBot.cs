b161129e0543e93b054c7471f7834a1a0169af32
Gianni Alessandroni <45824238+NotManyIdeasDev@users.noreply.github.com>
2023-08-14 11:25:19 +0200

    Refactor, removed buggy Move-Ordering
    Next thing to implement is TT

--- MyBot.cs
+++ MyBot.cs
@@ -1,120 +1,83 @@
 using ChessChallenge.API;
 using System;
-using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
-    //Pre-Generated PSTs and constants.
-    int bigNumber = 99999999;
-    int[] mgPieceValues = { 82, 337, 365, 477, 1025, 32000 }; //Pawn, Knight, Bishop, Rook, Queen, King
-    static ulong[,] compressedMGPSTs = {
-        { 0x0000000000000000, 0xDDFFECE9F11826EA, 0xE6FCFCF6030321F4, 0xE5FEFB0C11060AE7, 0xF20D0615170C11E9, 0xFA071A1F413819EC, 0x627F3D5F447E22F5, 0x0000000000000000 }, //mgPawnTable.
-        { 0x97EBC6DFEFE4EDE9, 0xE3CBF4FDFF12F2ED, 0xE9F70C0A131119F0, 0xF304100D1C1315F8, 0xF711133525451216, 0xD13C25415481492C, 0xB7D74824173E07EF, 0x81A7DECF3D9FF195 }, //mgKnightTable.
-        { 0xDFFDF2EBF3F4D9EB, 0x040F100007152101, 0x000F0F0F0E1B120A, 0xFA0D0D1A220C0A04, 0xFC051332252507FE, 0xF0252B28233225FE, 0xE610EEF31E3B12D1, 0xE304AEDBE7D607F8 }, //mgBishopTable.
-        { 0xEDF301111007DBE6, 0xD4F0ECF7FF0BFAB9, 0xD3E7F0EF0300FBDF, 0xDCE6F4FF09F906E9, 0xE8F5071A1823F8EC, 0xFB131A24112D3D10, 0x1B203A3E50431A2C, 0x202A20333F091F2B }, //mgRookTable.
-        { 0xFFEEF70AF1E7E1CE, 0xDDF80B02080FFD01, 0xF202F5FEFB020E05, 0xF7E6F7F6FEFC03FD, 0xE5E5F0F0FF11FE01, 0xF3EF07081D382F39, 0xE8D9FB01F0391C36, 0xE4001D0C3B2C2B2D }, //mgQueenTable.
-        { 0xF1240CCA08E4180E, 0x0107F8C0D5F00908, 0xF2F2EAD2D4E2F1E5, 0xCFFFE5D9D2D4DFCD, 0xEFECF4E5E2E7F2DC, 0xF71802F0EC0616EA, 0x1DFFECF9F8FCDAE3, 0xBF1710F1C8DE020D }, //mgKingTable.
+    const int infinity = 99999999;
+    int[] mgPieceValues = { 82, 337, 365, 477, 1025, 32000 }; //P, N, B, R, Q, K
+    ulong[,] compressedMGPSTs = {
+        { 0x0000000000000000, 0xDDFFECE9F11826EA, 0xE6FCFCF6030321F4, 0xE5FEFB0C11060AE7, 0xF20D0615170C11E9, 0xFA071A1F413819EC, 0x627F3D5F447E22F5, 0x0000000000000000 },
+        { 0x97EBC6DFEFE4EDE9, 0xE3CBF4FDFF12F2ED, 0xE9F70C0A131119F0, 0xF304100D1C1315F8, 0xF711133525451216, 0xD13C25415481492C, 0xB7D74824173E07EF, 0x81A7DECF3D9FF195 },
+        { 0xDFFDF2EBF3F4D9EB, 0x040F100007152101, 0x000F0F0F0E1B120A, 0xFA0D0D1A220C0A04, 0xFC051332252507FE, 0xF0252B28233225FE, 0xE610EEF31E3B12D1, 0xE304AEDBE7D607F8 },
+        { 0xEDF301111007DBE6, 0xD4F0ECF7FF0BFAB9, 0xD3E7F0EF0300FBDF, 0xDCE6F4FF09F906E9, 0xE8F5071A1823F8EC, 0xFB131A24112D3D10, 0x1B203A3E50431A2C, 0x202A20333F091F2B },
+        { 0xFFEEF70AF1E7E1CE, 0xDDF80B02080FFD01, 0xF202F5FEFB020E05, 0xF7E6F7F6FEFC03FD, 0xE5E5F0F0FF11FE01, 0xF3EF07081D382F39, 0xE8D9FB01F0391C36, 0xE4001D0C3B2C2B2D },
+        { 0xF1240CCA08E4180E, 0x0107F8C0D5F00908, 0xF2F2EAD2D4E2F1E5, 0xCFFFE5D9D2D4DFCD, 0xEFECF4E5E2E7F2DC, 0xF71802F0EC0616EA, 0x1DFFECF9F8FCDAE3, 0xBF1710F1C8DE020D },
     };
 
-    // victim: K, Q, R, B, N, P, None; attacker: None, K, Q, R, B, N, P
-    static ulong[] compressedMVVLVA = { 0x00000000000000, 0x0A0B0C0D0E0F00, 0x14151617181900, 0x1E1F2021222300, 0x28292A2B2C2D00, 0x32333435363700, 0x00000000000000 };
-
-    int botDepth = 4;
-    Move bestMove;
+    const byte botDepth = 4;
+    Move moveToPlay;
 
     public Move Think(Board board, Timer timer)
     {
-        SearchABNegaMax(board, botDepth, -bigNumber, bigNumber, board.IsWhiteToMove ? 1 : -1);
-        return bestMove;
+
+        Search(board, botDepth, -infinity, infinity, board.IsWhiteToMove ? 1 : -1);
+        return moveToPlay;
     }
 
-    public int EvaluatePosition(Board board)
+    public int Evaluate(Board board)
     {
         int evaluation = 0;
-        for (int i = 0; ++i < 7;)
+        for (byte i = 0; ++i < 7;)
         {
-            PieceList whitePieceList = board.GetPieceList((PieceType)i, true);
-            foreach (Piece whitePiece in whitePieceList)
-            {
-                //Material + piece placement
-                evaluation += mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, whitePiece.Square.Index, true);
-            }
+            PieceList whitePL = board.GetPieceList((PieceType)i, true);
+            foreach (Piece whiteP in whitePL)
+                evaluation += mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, whiteP.Square.Index, true);
 
-            PieceList blackPieceList = board.GetPieceList((PieceType)i, false);
-            foreach (Piece blackPiece in blackPieceList)
-            {
-                //Material + piece placement
-                evaluation -= mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, blackPiece.Square.Index, false);
-            }
+            PieceList blackPL = board.GetPieceList((PieceType)i, false);
+            foreach (Piece blackP in blackPL)
+                evaluation -= mgPieceValues[i - 1] + GetMGPSTValue((PieceType)i, blackP.Square.Index, false);
 
-            //Detect bishop pair, give 50 centipawns.
             if (i == 3)
-                evaluation += (50 * Convert.ToInt16(whitePieceList.Count == 2)) - (50 * Convert.ToInt16(blackPieceList.Count == 2));
+                evaluation += (50 * Convert.ToInt16(whitePL.Count == 2)) - (50 * Convert.ToInt16(blackPL.Count == 2));
         }
 
         return evaluation;
     }
 
-    //Move Ordering
-    public void OrderMoves(ref Move[] moves)
+    public int Search(Board board, int depth, int alpha, int beta, int color)
     {
-        List<Tuple<Move, int>> orderedMoves = new();
-        foreach (Move move in moves) { orderedMoves.Add(new Tuple<Move, int>(move, GetMVVLVAValue(move.CapturePieceType, move.MovePieceType))); }       
-
-        orderedMoves.Sort((a, b) =>  b.Item2.CompareTo(a.Item2));
-        for (int i = 0; i < moves.Length; i++) moves[i] = orderedMoves[i].Item1;
-
-        orderedMoves.Reverse();
-    }
-
-    public int SearchABNegaMax(Board board, int depth, int alpha, int beta, int color)
-    {
-        Move[] legalMoves;
-
-        if (board.IsInsufficientMaterial() || board.IsRepeatedPosition() || board.FiftyMoveCounter >= 100)
+        if (board.IsInStalemate() || board.IsFiftyMoveDraw() || board.IsRepeatedPosition() || board.IsInsufficientMaterial())
             return 0;
 
-        if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
-        {
-            if (board.IsInCheckmate())
-                return -9999999;
+        Move[] moves;
 
-            return color * EvaluatePosition(board);
-        }
+        if (depth == 0 || (moves = board.GetLegalMoves()).Length == 0)
+            return board.IsInCheckmate() ? board.PlyCount - 9999999 : (color * Evaluate(board));
 
-        OrderMoves(ref legalMoves);
-
-        int bestEvaluation = -bigNumber;
-        foreach (Move move in legalMoves)
+        int maxEvaluation = -infinity;
+        foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int evaluation = -SearchABNegaMax(board, depth - 1, -beta, -alpha, -color);
+            int evaluation = -Search(board, depth - 1, -beta, -alpha, -color);
             board.UndoMove(move);
 
-            if (evaluation > bestEvaluation)
+            if (evaluation > maxEvaluation)
             {
-                bestEvaluation = evaluation;
+                maxEvaluation = evaluation;
                 if (depth == botDepth)
-                    bestMove = move;
+                    moveToPlay = move;
             }
-            alpha = Math.Max(alpha, bestEvaluation);
+
+            alpha = Math.Max(alpha, evaluation);
             if (alpha >= beta) break;
         }
 
-        return bestEvaluation;
+        return maxEvaluation;
     }
 
     public int GetMGPSTValue(PieceType pieceType, int square, bool white)
     {
-        //Flip PSTs for opposite colors.
         int firstIndex = white ? (sbyte)(square / 8) : (7 - (sbyte)(square / 8));
-        //Get the corresponding positional value for each square of each pieceType, flip if black.
         return (sbyte)BitConverter.GetBytes(compressedMGPSTs[(byte)pieceType - 1, firstIndex])[7 - (square % 8)];
-    }
-
-    //Get corresponding encoded MVV-LVA Values
-    public int GetMVVLVAValue(PieceType victim, PieceType attacker)
-    {
-        return BitConverter.GetBytes(compressedMVVLVA[(int)victim])[(int)attacker];
     }
 }
